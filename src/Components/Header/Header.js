c827531f66a2c0981acdf1ae456923de95da9ad2
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-08-10 10:56:28 -0400

    add link to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 // import React from 'react'
+import { Link, Route } from "react-router-dom";
 
 function Header(props) {
   return (
@@ -7,9 +8,12 @@ function Header(props) {
         <i className="far fa-address-card"></i>
         <h1>Contacts</h1>
       </div>
-      <div className="header-right" onClick={e => props.addContact()}>
-        <i className="fas fa-plus"></i>
-      </div>
+      {/* <div className="header-right" onClick={e => props.addContact()}> */}
+      <Link to='/add-contact' className='link'>
+        <div className="header-right">
+          <i className="fas fa-plus"></i>
+        </div>
+      </Link>
     </div>
   )
 }
